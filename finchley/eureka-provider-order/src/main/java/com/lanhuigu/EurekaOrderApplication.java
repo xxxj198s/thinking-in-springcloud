26e31e4e7e30c11686b8afbed849dcd3340d491b
yihonglei <yihonglei@bcpeople.com>
2019-06-19 13:35:03 +0800

    provider

--- finchley/eureka-provider-order/src/main/java/com/lanhuigu/EurekaOrderApplication.java
+++ finchley/eureka-provider-order/src/main/java/com/lanhuigu/EurekaOrderApplication.java
@@ -1,5 +1,9 @@
 package com.lanhuigu;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 /**
  * @SpringBootApplication 启动一个Spring Boot应用程序
  * @EnableDiscoveryClient 服务发现与注册，当应用启动时，将应用注册到配置的注册中心
@@ -7,11 +11,6 @@ package com.lanhuigu;
  * @auther: yihonglei
  * @date: 2019-06-17 22:48
  */
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
 @EnableDiscoveryClient
 @SpringBootApplication
 public class EurekaOrderApplication {
