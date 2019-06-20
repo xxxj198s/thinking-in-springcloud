8ededd8ce53428d60af0f50eeaf58cd4710ee093
yihonglei <yihonglei@bcpeople.com>
2019-06-20 19:15:52 +0800

    代码恢复

--- finchley/eureka-register-available/src/main/java/com/lanhuigu/controller/HelloController.java
+++ finchley/eureka-register-available/src/main/java/com/lanhuigu/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.lanhuigu;
+package com.lanhuigu.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
