c69396d9a7fdf6b1ceced3beca90f64420a3b09d
yihonglei <yihonglei@bcpeople.com>
2019-07-01 11:46:22 +0800

    注释完善

--- finchley/eureka-feign-api/src/main/java/com/lanhuigu/order/OrderApi.java
+++ finchley/eureka-feign-api/src/main/java/com/lanhuigu/order/OrderApi.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * order服务
+ * order服务，FeignClient标注服务提供者信息
  *
  * @auther: yihonglei
  * @date: 2019-06-30 21:17
